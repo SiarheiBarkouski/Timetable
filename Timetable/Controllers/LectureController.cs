c4ecda28a67c13721077b2291a881a09634eb2c4
st <st@A210PC14>
2018-03-15 18:57:49 +0300

    Created.

---
+++ Timetable/Controllers/LectureController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Timetable.Controllers
+{
+    public class LectureController : Controller
+    {
+        // GET: Lecture
+        public string Index()
+        {
+            return "LectureController";
+        }
+    }
+}
