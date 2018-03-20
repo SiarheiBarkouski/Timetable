63522f70102b40c92540359a27397f3862884d31
Siarhei Barkouski <serega_10.10.93@mail.ru>
2018-03-20 20:57:32 +0300

    ToDo saving to excel.

--- Timetable/Controllers/LecturerController.cs
+++ Timetable/Controllers/LecturerController.cs
@@ -1,7 +1,11 @@
-﻿using System;
+﻿using OfficeOpenXml;
+using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Web;
+using System.Web.Hosting;
 using System.Web.Mvc;
 using Timetable.DbContextModels.Abstract;
 using Timetable.Models;
@@ -61,7 +65,89 @@ namespace Timetable.Controllers
 
             return View(records);
         }
-        
+
+        public FileResult DownloadDeals(string dateFrom, string dateTo, int instrumentId)
+        {
+            DateTime dtFrom = DateTime.Now.Date;
+            DateTime dtTo = DateTime.Now.Date;
+            if (dateFrom != null && dateTo != null)
+            {
+                dtFrom = DateTime.ParseExact(dateFrom, "dd.MM.yyyy", CultureInfo.InvariantCulture);
+                dtTo = DateTime.ParseExact(dateTo, "dd.MM.yyyy", CultureInfo.InvariantCulture);
+            }
+            dtTo = dtTo.AddDays(1);
+            tryDeleteOldImages();
+
+            var fileName = "Timetable_" + DateTime.Now.ToString("yyyyMMdd_HHmmssfff") + ".xlsx";
+            var outputDir = HostingEnvironment.MapPath("~/Content/DealsExcels/");
+
+            var file = new FileInfo(outputDir + fileName);
+            using (ExcelPackage xlPackage = new ExcelPackage(file))
+            {
+                xlPackage.Workbook.Worksheets.Add("Расписание");
+                var worksheet = xlPackage.Workbook.Worksheets[1];
+                worksheet.Column(1).Width = 20;
+                worksheet.Column(2).Width = 20;
+                worksheet.Column(3).Width = 20;
+                worksheet.Column(4).Width = 20;
+                worksheet.Column(5).Width = 20;
+                worksheet.Cells["A1:E1"].Style.Font.Bold = true;
+                worksheet.Cells["A1"].Value = "Инструмент";
+                worksheet.Cells["B1"].Value = "Дата сделки";
+                worksheet.Cells["C1"].Value = "Курс";
+                worksheet.Cells["D1"].Value = "Лот";
+                worksheet.Cells["E1"].Value = "Объем";
+
+                using (var context = new bcse_webEntities())
+                {
+
+                    var listOfDeals =
+                    (
+                        from instrument in context.Instruments
+                        join deal in context.BcseOnlineDeals on instrument.Id equals deal.InstrumentId
+                        where (deal.DealDate > dtFrom && deal.DealDate < dtTo) && deal.InstrumentId == instrumentId
+                        orderby deal.DealDate descending
+                        select new { instrumentName = instrument.Name, deal.DealDate, deal.Course, deal.LotAmount, deal.AttAmount, deal.InstrumentId }
+                    ).ToList();
+
+                    int i = 2;
+                    foreach (var d in listOfDeals)
+                    {
+                        worksheet.Cells["A" + i].Value = d.instrumentName;
+                        worksheet.Cells["B" + i].Value = d.DealDate.ToString();
+                        worksheet.Cells["C" + i].Value = d.Course;
+                        worksheet.Cells["D" + i].Value = d.LotAmount;
+                        worksheet.Cells["E" + i++].Value = d.AttAmount;
+                    }
+
+                }
+                xlPackage.Save();
+            }
+
+            byte[] fileBytes = System.IO.File.ReadAllBytes(HostingEnvironment.MapPath("~/Content/DealsExcels/" + fileName));
+            return File(fileBytes, System.Net.Mime.MediaTypeNames.Application.Octet, fileName);
+        }
+
+        private void tryDeleteOldImages()
+        {
+            string[] files = null;
+            try
+            {
+                files = Directory.GetFiles(HostingEnvironment.MapPath("~/Content/DealsExcels/"));
+            }
+            catch
+            {
+                Directory.CreateDirectory(HostingEnvironment.MapPath("~/Content/DealsExcels/"));
+                files = Directory.GetFiles(HostingEnvironment.MapPath("~/Content/DealsExcels/"));
+            }
+            foreach (string file in files)
+            {
+                FileInfo fi = new FileInfo(file);
+                if (DateTime.Now.Subtract(fi.CreationTime).Minutes > 5)
+                    fi.Delete();
+            }
+        }
+
         public int GetCurrentWeek()
         {
             var dtNow = new DateTime();
